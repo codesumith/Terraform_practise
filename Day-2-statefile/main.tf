a9100cda15167b69b7b738b305c3549f43f93599
codesumith <sumithkumar100@gmail.com>
2025-04-17 13:18:13 +0530

    day2 practise of state file changes

---
+++ Day-2-statefile/main.tf
@@ -0,0 +1,10 @@
+resource "aws_instance" "name" {
+        
+        ami = var.ami_id
+        instance_type = var.instance_type
+
+        tags = {
+           Name = "MyEc2Instance"
+        }       
+
+}
---
+++ Day-2-statefile/variable.tf
@@ -0,0 +1,10 @@
+variable "ami_id" {
+    type = string
+    default = "ami-00a929b66ed6e0de6"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t2.micro"
+  
+}
