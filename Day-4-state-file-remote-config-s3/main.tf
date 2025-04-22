7115a7f788974fe9ad37d1875ce8edeca412d001
codesumith <sumithkumar100@gmail.com>
2025-04-22 14:01:47 +0530

    day4 changes

--- Day-4-state-file-remote-config-s3/main.tf
+++ Day-4-state-file-remote-config-s3/main.tf
@@ -14,8 +14,8 @@ resource "aws_subnet" "test_sb_1b" {
     availability_zone = "us-east-1b"
 }
 
-resource "aws_subnet" "prob_sb_1c" {
+resource "aws_subnet" "uat_sb_1d" {
     vpc_id = aws_vpc.dev_vpc.id
-    cidr_block = "10.0.2.0/24"
-    availability_zone = "us-east-1c"
+    cidr_block = "10.0.3.0/24"
+    availability_zone = "us-east-1d"
 }
