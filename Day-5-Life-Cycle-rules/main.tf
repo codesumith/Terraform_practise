482cad8f805042bee545e2240836c447db4f8bd1
codesumith <sumithkumar100@gmail.com>
2025-04-23 13:03:58 +0530

    day5 life cycle rules examples

--- Day-5-Life-Cycle-rules/main.tf
+++ Day-5-Life-Cycle-rules/main.tf
@@ -1,16 +1,19 @@
 resource "aws_instance" "name" {
     ami = "ami-00a929b66ed6e0de6"
     instance_type = "t2.micro"
-    availability_zone = "us-east-1a"
+    availability_zone = "us-east-1b"
 
-    tags = {
-      Name = "ravi"
-    }
+
 
     lifecycle {
-      # prevent_destroy = true
-      # ignore_changes = [ tags ]
-      # create_before_destroy = true
+      # If you change something like the availability zone, 
+      # subnet, or attached volumes, Terraform might have to 
+      # destroy the original instance before creating the new 
+      # one — especially if the AMI or instance configuration requires it.
+       create_before_destroy = true 
+       ignore_changes = [ tags ]
+      #  prevent_destroy = true
+
     }
 
    
