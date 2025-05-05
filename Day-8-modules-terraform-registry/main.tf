ffeced9f980324dd555efad284f5772539340dbe
codesumith <sumithkumar100@gmail.com>
2025-05-05 21:49:29 +0530

    day-8-modules

---
+++ Day-8-modules-terraform-registry/main.tf
@@ -0,0 +1,13 @@
+module "terrafrom_registry_s3" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
